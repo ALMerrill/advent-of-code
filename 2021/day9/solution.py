ea6a3f4b37e9b62b19aa39867b0c507bf0f8c233
almerrill <merrillman32@gmail.com>
2021-12-08 23:26:14 -0700

    finished part 2

--- 2021/day9/solution.py
+++ 2021/day9/solution.py
@@ -1,5 +1,7 @@
 import sys; sys.path.append("../"); from utils import *
 import numpy as np
+import operator
+import functools
 
 def is_in_matrix(matrix, position):
     i = position[0]
@@ -25,7 +27,7 @@ def is_low_point(i, j, heights):
     
     
 
-lines = read_file_lines("sample.txt")
+lines = read_file_lines("input.txt")
 
 heights = np.array([[char for char in line] for line in lines]).astype(int)
 
@@ -41,41 +43,33 @@ print("Part 1:", (np.array(low_values) + 1).sum() )
 
 
 
-def search(matrix, i, j, count, visited, counts, points):
+def search(matrix, i, j, count, visited, points):
     if (i,j) in visited:
-        counts.append(count)
         return count
     visited.append((i, j))
     if not is_in_matrix(matrix, (i, j)) or matrix[i, j] == 9:
-        counts.append(count)
         return count
     else:
         points.append((i,j))
         count += 1 
         for position in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
-            search(matrix, position[0], position[1], count, visited, counts, points)
+            search(matrix, position[0], position[1], count, visited, points)
     return count
 
-
+basin_sizes = []
 for low_point in low_points:
     i, j = low_point
-    print("Point:", i, j)
     count = 0
     visited = []
-    counts = []
     points = []
-    search(heights, i, j, count, visited, counts, points)
-    print(max(counts))
-    print(len(points))
-#for low_point in low_points:
-#    print("POINT:", low_point)
-#    i, j = low_point
-#    count = 1
-#    visited = []
-#    counts = []
-#    search(heights, i, j, count, visited, counts)
-#    print(max(counts))
-
-
-breakpoint()
-print("Part 2:", "?")
+    search(heights, i, j, count, visited, points)
+    basin_size = len(points)
+    if len(basin_sizes) == 3:
+        largers = [basin_size > size for size in basin_sizes]
+        if sum([int(larger) for larger in largers]) > 0:
+            index = largers.index(True)
+            del basin_sizes[index]
+            basin_sizes.append(basin_size)
+    else:
+        basin_sizes.append(basin_size)
+print("Part 2:", functools.reduce(operator.mul, basin_sizes, 1))
